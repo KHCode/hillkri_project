7851e180322ba6f3ea36104d565a75add5471866
KHCode <hillkri@oregonstate.edu>
2020-11-27 21:52:02 -0800

    add models files, connect app to routers

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var teamsRouter = require('./routes/teams');
+var polsRouter = require('./routes/pols');
+var instsRouter = require('./routes/insts')
 
 var app = express();
 
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users/:user_id', usersRouter);
+app.use('/pols', polsRouter);
+app.use('/insts', instsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/teams.js
@@ -0,0 +1,19 @@
+async function post_team(req, res, next) {
+
+}
+
+async function get_teams(req, res, next) {
+
+}
+
+async function get_a_team(req, res, next) {
+
+}
+
+async function patch_a_team(req, res, next) {
+    
+}
+
+async function put_a_team(req, res, next) {
+
+}
